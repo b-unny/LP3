1441ff78f86950ba87006694cba423175504e583
Camilla Matos <146495739+b-unny@users.noreply.github.com>
2024-10-01 10:05:33 -0300

    Update Robo.java

--- Robo.java
+++ Robo.java
@@ -2,6 +2,14 @@ public class Robo{
     private double posX;
     private double posY;
     private double vMedia;
+
+    Robo(){
+        posX = 0.0;
+        posY = 0.0;
+    }
     
-    Robo(double pos)
+    Robo(double posX, double posY){
+        this.posX = posX;
+        this.posY = posY;
+    }
 }
